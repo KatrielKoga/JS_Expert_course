08297807a727ae3794814dd46d5a9f3b69a065ab
KatrielKoga <katriel.koga.2012@hotmail.com>
2022-06-07 07:05:21 -0300

    finalizacao projeto final

--- modulo3/demo2-esmodules-internationalization/src/index.js
+++ modulo3/demo2-esmodules-internationalization/src/index.js
@@ -1,8 +1,11 @@
 import database from './../database.json';
 import Person from './person.js';
 import TerminalController from './terminalController.js';
-
+import { save } from './repository.js';
 const DEFAULT_LANG = 'pt-BR';
+// const DEFAULT_LANG = 'es';
+// const DEFAULT_LANG = 'en';
+// const DEFAULT_LANG = 'rus';
 const STOP_TERM = ':q';
 
 const terminalController = new TerminalController();
@@ -17,7 +20,8 @@ async function mainLoop() {
 			return;
 		}
 		const person = Person.generateInstanceFromString(answer);
-		console.log('person', person.formatted(DEFAULT_LANG));
+		terminalController.updateTable(person.formatted(DEFAULT_LANG));
+		await save(person);
 		return mainLoop();
 	} catch (error) {
 		console.log('DEU RUIM**', error);
