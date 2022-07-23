7688f2b692f349730a94290f96297719b4b7bd19
KatrielKoga <katriel.koga.2012@hotmail.com>
2022-07-23 07:47:05 -0300

    padrao observer

---
+++ modulo7/behavioral/02.observer/test/index.test.js
@@ -0,0 +1,66 @@
+import { expect, describe, test, jest, beforeAll } from '@jest/globals';
+import Payment from '../src/events/payment';
+import Marketing from '../src/observers/marketing';
+import Shipment from '../src/observers/shipment';
+import PaymentSubject from '../src/subjects/paymentSubject';
+
+describe('Test Suite for Observer Pattern', () => {
+	beforeAll(() => {
+		jest.spyOn(console, console.log.name).mockImplementation(() => {});
+	});
+	test('#PaymentSubject notify observers', () => {
+		const subject = new PaymentSubject();
+		const observer = {
+			update: jest.fn(),
+		};
+		const data = 'hello world';
+		const expected = data;
+		subject.subscribe(observer);
+		subject.notify(data);
+		expect(observer.update).toBeCalledWith(expected);
+	});
+	test('#PaymentSubject should not notify unsubscribed observers', () => {
+		const subject = new PaymentSubject();
+		const observer = {
+			update: jest.fn(),
+		};
+		const data = 'hello world';
+
+		subject.subscribe(observer);
+		subject.unsubscribe(observer);
+		subject.notify(data);
+		expect(observer.update).not.toHaveBeenCalled();
+	});
+	test('#Payment should nofity subject after a credit card transaction', () => {
+		const subject = new PaymentSubject();
+		const payment = new Payment(subject);
+
+		const paymentSubjectNofifierSpy = jest.spyOn(
+			payment.paymentSubject,
+			payment.paymentSubject.notify.name
+		);
+		const data = { userName: 'katrielkoga', id: Date.now() };
+		payment.creditCard(data);
+
+		expect(paymentSubjectNofifierSpy).toBeCalledWith(data);
+	});
+	test('#All should netify subscribers after a credit card transaction', () => {
+		const subject = new PaymentSubject();
+		const shipment = new Shipment();
+		const marketing = new Marketing();
+
+		const shipmentSpy = jest.spyOn(shipment, shipment.update.name);
+		const marketingSpy = jest.spyOn(marketing, marketing.update.name);
+
+		subject.subscribe(shipment);
+		subject.subscribe(marketing);
+
+		const payment = new Payment(subject);
+		const data = { userName: 'katrielkoga', id: Date.now() };
+
+		payment.creditCard(data);
+
+		expect(shipmentSpy).toBeCalledWith(data);
+		expect(marketingSpy).toBeCalledWith(data);
+	});
+});
