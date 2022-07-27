355e96781ab1a0026e5ee851cacd726b92f7bd4c
KatrielKoga <katriel.koga.2012@hotmail.com>
2022-07-27 07:24:21 -0300

    template method pattern

---
+++ modulo7/behavioral/03.template-method/test/orderBusiness.test.js
@@ -0,0 +1,51 @@
+import { expect, describe, test, jest, beforeEach } from '@jest/globals';
+import OrderBusiness from '../src/business/orderBusiness.js';
+import Order from '../src/entities/order.js';
+
+describe('Test suite for Template Method design pattern', () => {
+	beforeEach(() => {
+		jest.restoreAllMocks();
+	});
+	describe('#OrderBusiness', () => {
+		test('execution Order Business without Template Method', () => {
+			const order = new Order({
+				customerId: 1,
+				amount: 100.0,
+				products: [{ description: 'ferrari' }],
+			});
+
+			const orderBusiness = new OrderBusiness();
+			// todos os dev's devem obrigatoriamente lembrar de seguir a risca esse fluxo de execucao
+			// se algum esquecer de chamar a funcao de validacao pode quebrar todo o sistema
+			const isValid = orderBusiness._validateRequiredFields(order);
+			expect(isValid).toBeTruthy();
+
+			const result = orderBusiness._create(order);
+			expect(result).toBeTruthy();
+		});
+		test('execution Order Business with Template Method', () => {
+			const order = new Order({
+				customerId: 1,
+				amount: 100.0,
+				products: [{ description: 'ferrari' }],
+			});
+
+			const orderBusiness = new OrderBusiness();
+			const calledValidationFn = jest.spyOn(
+				orderBusiness,
+				orderBusiness._validateRequiredFields.name
+			);
+			const calledCreateFn = jest.spyOn(
+				orderBusiness,
+				orderBusiness._create.name
+			);
+
+			// com template method a sequencia de passos é sempre executada
+			// evita a replicacao de logica
+			const result = orderBusiness.create(order);
+			expect(result).toBeTruthy();
+			expect(calledValidationFn).toHaveBeenCalled();
+			expect(calledCreateFn).toHaveBeenCalled();
+		});
+	});
+});
