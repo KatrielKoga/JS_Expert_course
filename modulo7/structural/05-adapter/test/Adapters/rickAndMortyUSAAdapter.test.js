34d6b4be4b9a7ffde0af83ed6c11fe29a9d06118
KatrielKoga <katriel.koga.2012@hotmail.com>
2022-08-04 08:08:34 -0300

    padrao adapter

---
+++ modulo7/structural/05-adapter/test/Adapters/rickAndMortyUSAAdapter.test.js
@@ -0,0 +1,15 @@
+import { expect, describe, test, jest, beforeEach } from '@jest/globals';
+import RickAndMortyUSAAdapter from '../../src/business/adapters/rickAndMortyUSAAdapter';
+import RickAndMortyUSA from '../../src/business/integrations/rickAndMortyUSA';
+
+describe('#RickAndMortyUSAAdapter', () => {
+	beforeEach(() => jest.clearAllMocks());
+	test('#getCharacters should be an adapter for RickAndMortyUSA.getCharactersJSON', async () => {
+		const USAIntegration = jest
+			.spyOn(RickAndMortyUSA, RickAndMortyUSA.getCharactersFromXML.name)
+			.mockResolvedValue([]);
+		const result = await RickAndMortyUSAAdapter.getCharacters();
+		expect(result).toEqual([]);
+		expect(USAIntegration).toHaveBeenCalled();
+	});
+});
