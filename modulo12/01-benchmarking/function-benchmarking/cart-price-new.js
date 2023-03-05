36bc89fe55772d4455572c8b6e1e8b03330cd3ae
KatrielKoga <katriel.koga.2012@hotmail.com>
2023-03-05 08:08:35 -0300

    modulo 12 performance aula 1 benchmarking

---
+++ modulo12/01-benchmarking/function-benchmarking/cart-price-new.js
@@ -0,0 +1,12 @@
+export default class Cart {
+	constructor({ at, products }) {
+		this.products = products;
+		this.total = this.getCartPrice();
+	}
+	getCartPrice() {
+		let price = 0;
+		for (const product of this.products) {
+			price += product.price;
+		}
+	}
+}
